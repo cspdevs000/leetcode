5867cadcded2756dbcf3cbadd333fd6dd6aca682
cspdevs000 <cspdevs000@gmail.com>
2022-03-12 22:49:44 -0500

    palindrome number

---
+++ python/palindromeNumber.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        
+        div = 1 
+        while x >= 10 * div:
+            div *= 10
+            
+        while x:
+            right = x % 10
+            left = x // div
+            
+            if left != right:
+                return False
+            
+            x = (x % div) // 10
+            div = div / 100
+            
+        return True
+
+    print(isPalindrome('x', 121))
+    print(isPalindrome('x', 1221))
+    print(isPalindrome('x', 12221))
+    print(isPalindrome('x', 12111))
+    print(isPalindrome('x', 121131))
+    print(isPalindrome('x', 123131))
