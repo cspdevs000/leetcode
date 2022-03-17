87247ec7fce16dbe717e2ca5bca2a30b96fd769a
cspdevs000 <cspdevs000@gmail.com>
2022-03-16 23:51:19 -0400

    longest substring without repeating characters py

--- python/longestSubstring.py
+++ python/longestSubstring.py
@@ -1,1 +1,19 @@
 #Longest Substring Without Repeating Characters
+
+def lengthOfLongestSubstring(self, s: str) -> int:
+    charSet = set()
+    l = 0
+    result = 0
+    
+    for r in range(len(s)):
+        while s[r] in charSet:
+            charSet.remove(s[l])
+            l += 1
+        charSet.add(s[r])
+        result = max (result, r - l + 1)
+    return result
+
+print(lengthOfLongestSubstring(lengthOfLongestSubstring, "abcabcbb"))
+print(lengthOfLongestSubstring(lengthOfLongestSubstring, "bbbbbb"))
+print(lengthOfLongestSubstring(lengthOfLongestSubstring, "pwwkew"))
+print(lengthOfLongestSubstring(lengthOfLongestSubstring, "lkadsfjljksdjflkasdjflkdasfjskljgjkaskjgjkksglkjfsakgjfslglasfgasflgkajsflgkjalfsjgjasfkfkk"))
