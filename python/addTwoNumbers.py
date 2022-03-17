9798f91d276d079ff6fe8612d28a9106ce5c4840
cspdevs000 <cspdevs000@gmail.com>
2022-03-16 23:42:46 -0400

    add two numbers py

--- python/addTwoNumbers.py
+++ python/addTwoNumbers.py
@@ -5,25 +5,28 @@ class ListNode:
         self.val = val
         self.next = next
 
-def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-    dummy = ListNode()
-    current = dummy
-    
-    carry = 0
-    while l1 or l2 or carry:
-        v1 = l1.val if l1 else 0
-        v2 = l2.val if l2 else 0
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        l1 = ListNode()
+        l2 = ListNode()
+        current = dummy
         
-        val = v1 + v2 + carry
-        carry = val // 10
-        val = val % 10
-        current.next = ListNode(val)
-        
-        current = current.next
-        l1 = l1.next if l1 else None
-        l2 = l2.next if l2 else None
-        
-    return dummy.next
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            current.next = ListNode(val)
+            
+            current = current.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            
+        return dummy.next
 
-addTwoNumbers(2,4,3)
-addTwoNumbers(5,6,4)
+    print(addTwoNumbers(2,4,3))
+    print(addTwoNumbers(5,6,4))
