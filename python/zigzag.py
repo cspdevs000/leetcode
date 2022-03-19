cde7213dd8f833b74932292cb000c57c12bc0594
cspdevs000 <cspdevs000@gmail.com>
2022-03-19 16:38:02 -0400

    reverse integer py

---
+++ python/zigzag.py
@@ -0,0 +1,17 @@
+def convert(self, s: str, numRows: int) -> str:
+    if numRows == 1: return s
+    
+    result = ""
+    for r in range(numRows):
+        increment = 2 * (numRows - 1)
+        for i in range(r, len(s), increment):
+            result += s[i]
+            if (r > 0 and r < numRows - 1 and 
+                i + increment - 2 * r < len(s)):
+                result += s[i + increment - 2 * r]
+            
+    return result
+
+print(convert(convert, "PAYPALISHIRING", 3))
+print(convert(convert, "PAYPALISHIRING", 4))
+print(convert(convert, "PAYPALISHIRING", 5))
