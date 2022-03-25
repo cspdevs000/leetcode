e5ece0f50f17a112982d118dad954b960b0bb6f6
cspdevs000 <cspdevs000@gmail.com>
2022-03-24 20:49:09 -0400

    container with most water py

--- python/containerWithMostWater.py
+++ python/containerWithMostWater.py
@@ -1,3 +1,5 @@
+from typing import List
+
 def maxArea(height: List[int]) -> int:
     result = 0
     l, r = 0, len(height) - 1
@@ -12,3 +14,7 @@ def maxArea(height: List[int]) -> int:
             r -= 1
 
     return result
+
+print(maxArea([1,8,6,2,5,4,8,3,7]))
+print(maxArea([1,1]))
+print(maxArea([5,2,4,6,7,9,3,2,11]))
