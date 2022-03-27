613183f067f9b3ab78a476bd3401710c0b7231ea
cspdevs000 <cspdevs000@gmail.com>
2022-03-26 21:00:00 -0400

    reverse integer js

--- javascript/scratchPad.js
+++ javascript/scratchPad.js
@@ -48,5 +48,21 @@ function fizzBuzz(num) {
     }
 }
 
-fizzBuzz(77);
+// fizzBuzz(77);
 
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+// console.log(reverseString('practicing javascript'))
+
+
+function reverseInteger(num) {
+    let str = String(num);
+    result = Number(str.split('').reverse().join(''));
+    return result;
+}
+
+console.log(reverseInteger(12));
+console.log(reverseInteger(123));
+console.log(reverseInteger(17756));
