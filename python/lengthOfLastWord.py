19ae1a5b363e392d55f049d226062a3ec3b3417e
cspdevs000 <cspdevs000@gmail.com>
2022-04-16 21:12:58 -0400

    length of last word py

--- python/lengthOfLastWord.py
+++ python/lengthOfLastWord.py
@@ -1,4 +1,6 @@
-def lengthOfLastWord(self, s):
+def lengthOfLastWord(s):
     return len(s.split()[-1])
 
-    
+print(lengthOfLastWord("Hello World"))
+print(lengthOfLastWord("   fly me   to   the moon  "))
+print(lengthOfLastWord("luffy is still joyboy"))
