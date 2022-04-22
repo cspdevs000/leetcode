edb5ae48fc0808ab2f9a067eb7dba2f9703e4daa
cspdevs000 <cspdevs000@gmail.com>
2022-04-21 20:45:13 -0400

    sqrt js

---
+++ javascript/sqrt.js
@@ -0,0 +1,25 @@
+var sqrt = function(x) {
+    let left = 1;
+    let right = x;
+    
+    if (x < 2) {
+        return x;
+    }
+    
+    while (left < right) {
+        let mid = left + Math.floor((right - left) / 2);
+    
+        if (mid * mid === x) {
+            return mid;
+        } else if (mid * mid > x) {
+            right = mid;
+        } else if (mid * mid < x) {
+            left = mid + 1;
+        }
+    }
+    
+    return left - 1
+};
+
+console.log(sqrt(4))
+console.log(sqrt(8))
